d7734fb3717a2c977e5200bd7603ca5eff8b3079
Chris Faria <christopher.faria@wwt.com>
2024-09-18 16:30:22 -0500

    improvements

--- process_test/vlan_configuration.py
+++ process_test/vlan_configuration.py
@@ -29,10 +29,18 @@ def configure_vlan(device, vlan_id=400):
         if device['connection_details']['method'] == 'SSH':
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(device['connection_details']['host'], 
-                        username=device['connection_details']['username'], 
-                        password=device['connection_details']['password'],
-                        look_for_keys=False, allow_agent=False)
+            
+            try:
+                ssh.connect(device['connection_details']['host'], 
+                            username=device['connection_details']['username'], 
+                            password=device['connection_details']['password'],
+                            look_for_keys=False, allow_agent=False)
+            except paramiko.AuthenticationException:
+                logging.error(f"Authentication failed for {device['hostname']}")
+                return
+            except paramiko.SSHException as e:
+                logging.error(f"SSH connection error to {device['hostname']}: {str(e)}")
+                return
 
             # Open interactive shell
             remote_conn = ssh.invoke_shell()
@@ -47,10 +55,26 @@ def configure_vlan(device, vlan_id=400):
             vlan_check_output = send_command(remote_conn, check_vlan_command, 2)
 
             if str(vlan_id) in vlan_check_output:
-                logging.info(f"VLAN {vlan_id} already exists on {device['hostname']}. Skipping configuration.")
+                logging.info(f"VLAN {vlan_id} already exists on {device['hostname']}.")
+                # Check detailed VLAN configuration
+                detailed_check_command = f"show running-config | section vlan {vlan_id}"
+                detailed_vlan_config = send_command(remote_conn, detailed_check_command, 2)
+                logging.info(f"Detailed VLAN {vlan_id} configuration on {device['hostname']}: {detailed_vlan_config}")
+
+                # Check if specific trunk ports allow the VLAN
+                for trunk_port in ["GigabitEthernet1/0/1", "GigabitEthernet1/0/2"]:
+                    trunk_check_command = f"show running-config interface {trunk_port} | include allowed vlan"
+                    trunk_config = send_command(remote_conn, trunk_check_command, 1)
+                    logging.info(f"Trunk port {trunk_port} VLAN config on {device['hostname']}: {trunk_config}")
+                    if f"allowed vlan {vlan_id}" in trunk_config:
+                        logging.info(f"Trunk port {trunk_port} allows VLAN {vlan_id} on {device['hostname']}.")
+                    else:
+                        logging.warning(f"Trunk port {trunk_port} does not allow VLAN {vlan_id} on {device['hostname']}.")
             else:
                 # If VLAN does not exist, enter configuration mode or config-transaction mode
-                output = send_command(remote_conn, "show version", 1)
+                output = send_command(remote_conn, "enable", 1)
+                output += send_command(remote_conn, "show version", 1)
+
                 if "Controller mode" in output:
                     logging.info(f"{device['hostname']} is in Controller mode, using config-transaction.")
                     send_command(remote_conn, "config-transaction", 1)
@@ -65,7 +89,7 @@ def configure_vlan(device, vlan_id=400):
                 send_command(remote_conn, "exit", 1)
 
                 # Update trunk ports (adjust based on your network)
-                for trunk_port in ["GigabitEthernet1/0/1", "GigabitEthernet1/0/2"]:  # Adjust based on your network
+                for trunk_port in ["GigabitEthernet1/0/1", "GigabitEthernet1/0/2"]:
                     trunk_command = f"interface {trunk_port}\nswitchport trunk allowed vlan add {vlan_id}"
                     logging.info(f"Running trunk port update command on {device['hostname']}: {trunk_command}")
                     send_command(remote_conn, trunk_command, 1)
