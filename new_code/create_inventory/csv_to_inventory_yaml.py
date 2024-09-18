bab6c6ce16cd0de2fda951f6478bb023c70319f8
Chris Faria <christopher.faria@wwt.com>
2024-09-18 11:36:45 -0500

    update

--- new_code/create_inventory/csv_to_inventory_yaml.py
+++ new_code/create_inventory/csv_to_inventory_yaml.py
@@ -8,6 +8,14 @@ def csv_to_yaml(csv_file, yaml_file):
     with open(csv_file, mode='r') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
+            connection_details = row['connection_details'].split(", ")
+
+            # Handle missing fields with default values (if a field is missing)
+            host = connection_details[0].split(": ")[1] if len(connection_details) > 0 else "N/A"
+            method = connection_details[1].split(": ")[1] if len(connection_details) > 1 else "N/A"
+            username = connection_details[2].split(": ")[1] if len(connection_details) > 2 else "N/A"
+            password = connection_details[3].split(": ")[1] if len(connection_details) > 3 else "N/A"
+
             device = {
                 "hostname": row['hostname'],
                 "site": row['site'],
@@ -15,10 +23,10 @@ def csv_to_yaml(csv_file, yaml_file):
                 "commands": row['commands'].split(";"),  # Split commands if multiple commands are separated by semicolons
                 "config_commands": row['config_commands'].split(";"),
                 "connection_details": {
-                    "host": row['connection_details'].split(", ")[0].split(": ")[1],
-                    "method": row['connection_details'].split(", ")[1].split(": ")[1],
-                    "username": row['connection_details'].split(", ")[2].split(": ")[1],
-                    "password": row['connection_details'].split(", ")[3].split(": ")[1]
+                    "host": host,
+                    "method": method,
+                    "username": username,
+                    "password": password
                 }
             }
             inventory["devices"].append(device)
