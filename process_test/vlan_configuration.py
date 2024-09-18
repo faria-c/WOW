05a9264ff23b3dc160e44d79b79a068527fa56b8
Chris Faria <christopher.faria@wwt.com>
2024-09-18 15:44:11 -0500

    update

--- process_test/vlan_configuration.py
+++ process_test/vlan_configuration.py
@@ -1,6 +1,7 @@
 import paramiko
 import yaml
 import logging
+import time
 
 # Set up logging
 logging.basicConfig(filename='vlan_configuration.log', level=logging.INFO)
@@ -17,64 +18,48 @@ def configure_vlan(device, vlan_id=400):
                         password=device['connection_details']['password'],
                         look_for_keys=False, allow_agent=False)
 
-            # Enable keepalive to prevent session drops
-            ssh.get_transport().set_keepalive(30)
+            # Open interactive shell
+            remote_conn = ssh.invoke_shell()
+            time.sleep(1)  # Give the shell time to initialize
 
-            # Enter configuration mode
-            stdin, stdout, stderr = ssh.exec_command("configure terminal")
-            config_output = stdout.read().decode()
-            config_error_output = stderr.read().decode()
-
-            if config_error_output:
-                logging.error(f"Error entering configuration mode on {device['hostname']}: {config_error_output}")
-                ssh.close()
-                return
+            # Enter enable mode if required
+            remote_conn.send("enable\n")
+            time.sleep(1)
+            remote_conn.send("configure terminal\n")
+            time.sleep(1)
 
             # Check if VLAN exists
-            check_vlan_command = f"show vlan brief | include {vlan_id}"
+            check_vlan_command = f"show vlan brief | include {vlan_id}\n"
             logging.info(f"Running VLAN check command on {device['hostname']}: {check_vlan_command}")
-            stdin, stdout, stderr = ssh.exec_command(check_vlan_command)
-            output = stdout.read().decode()
-            error_output = stderr.read().decode()
-
-            if error_output:
-                logging.error(f"Error during VLAN check on {device['hostname']}: {error_output}")
-                ssh.close()
-                return
-
+            remote_conn.send(check_vlan_command)
+            time.sleep(2)  # Wait for the command to execute
+            
+            output = remote_conn.recv(65535).decode("utf-8")
             logging.info(f"VLAN check output for {device['hostname']}: {output}")
-
+            
             if str(vlan_id) in output:
                 logging.info(f"VLAN {vlan_id} already exists on {device['hostname']}.")
             else:
                 logging.info(f"VLAN {vlan_id} does not exist on {device['hostname']}. Creating VLAN.")
                 
-                # Send the VLAN creation command
-                stdin, stdout, stderr = ssh.exec_command(f"vlan {vlan_id}")
-                vlan_output = stdout.read().decode()
-                vlan_error_output = stderr.read().decode()
+                # Create VLAN
+                remote_conn.send(f"vlan {vlan_id}\n")
+                time.sleep(1)
+                remote_conn.send("exit\n")
+                time.sleep(1)
 
-                if vlan_error_output:
-                    logging.error(f"Error creating VLAN on {device['hostname']}: {vlan_error_output}")
-                else:
-                    logging.info(f"VLAN {vlan_id} created successfully on {device['hostname']}.")
-
-                # Update trunk ports
+                # Update trunk ports (this is a simplified example)
                 for trunk_port in ["GigabitEthernet1/0/1", "GigabitEthernet1/0/2"]:  # Adjust based on your network
-                    trunk_command = f"interface {trunk_port}\nswitchport trunk allowed vlan add {vlan_id}\nexit"
+                    trunk_command = f"interface {trunk_port}\nswitchport trunk allowed vlan add {vlan_id}\nexit\n"
                     logging.info(f"Running trunk port update command on {device['hostname']}: {trunk_command}")
-                    stdin, stdout, stderr = ssh.exec_command(trunk_command)
+                    remote_conn.send(trunk_command)
+                    time.sleep(1)
 
-                    trunk_output = stdout.read().decode()
-                    trunk_error_output = stderr.read().decode()
-
-                    if trunk_error_output:
-                        logging.error(f"Error updating trunk port {trunk_port} on {device['hostname']}: {trunk_error_output}")
-                    else:
-                        logging.info(f"Trunk port {trunk_port} updated successfully on {device['hostname']}.")
+                logging.info(f"VLAN {vlan_id} and trunk configuration completed on {device['hostname']}.")
 
             # Exit configuration mode
-            stdin, stdout, stderr = ssh.exec_command("end")
+            remote_conn.send("end\n")
+            time.sleep(1)
             ssh.close()
 
     except Exception as e:
