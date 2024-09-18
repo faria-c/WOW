201960d948354712e0108fd6f71cd86c6aa70977
Chris Faria <christopher.faria@wwt.com>
2024-09-18 14:17:22 -0500

    update

--- new_code/create_inventory/csv_to_inventory_yaml.py
+++ new_code/create_inventory/csv_to_inventory_yaml.py
@@ -7,17 +7,39 @@ def csv_to_yaml(csv_file, yaml_file):
     with open(csv_file, mode='r') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
+            # Parse connection details field
+            connection_details = row['connection_details'].split(", ")
+
+            # Initialize default values
+            host, method, username, password = "N/A", "N/A", "N/A", "N/A"
+
+            # Parse each detail
+            for detail in connection_details:
+                if "host" in detail:
+                    host = detail.split(": ")[1] if len(detail.split(": ")) > 1 else "N/A"
+                elif "method" in detail:
+                    method = detail.split(": ")[1] if len(detail.split(": ")) > 1 else "N/A"
+                elif "username" in detail:
+                    username = detail.split(": ")[1] if len(detail.split(": ")) > 1 else "N/A"
+                elif "password" in detail:
+                    password = detail.split(": ")[1] if len(detail.split(": ")) > 1 else "N/A"
+
+            # Create device dictionary
             device = {
                 "hostname": row['hostname'],
-                "management_ip": row['management_ip'],
-                "connection_method": row['connection_method'],
+                "site": row['site'],
+                "roles": row['roles'],
+                "management_ip": host,
+                "connection_method": method,
                 "credentials": {
-                    "username": row['username'],
-                    "password": row['password']
+                    "username": username,
+                    "password": password
                 }
             }
+
             inventory["devices"].append(device)
 
+    # Write inventory to YAML file
     with open(yaml_file, 'w') as file:
         yaml.dump(inventory, file, default_flow_style=False)
 
